95f738caa784a2754546d51db514a6c3b8c21c0b
Mustafa Bacanak <bacanak117@gmail.com>
2022-12-25 00:19:25 +0300

    Maven & JUnit

--- src/test/java/day06/C01_Dropdown.java
+++ src/test/java/day06/C01_Dropdown.java
@@ -1,6 +1,8 @@
 package day06;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -23,13 +25,12 @@ public class C01_Dropdown {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://testcenter.techproeducation.com/index.php?page=dropdown");
+
     }
 
     @Test
     public void selectByIndexTest(){
-
-        // https://testcenter.techproeducation.com/index.php?page=dropdown
-        driver.get("https://testcenter.techproeducation.com/index.php?page=dropdown");
 
         // 1. LOCATE dropdown element
         WebElement year = driver.findElement(By.xpath("//select[@id='year']"));
@@ -53,33 +54,51 @@ public class C01_Dropdown {
 
     @Test
     public void printAllTest(){
-        driver.get("https://testcenter.techproeducation.com/index.php?page=dropdown");
-
         // Tum eyaletleri consala yazdıralım
-        WebElement state = driver.findElement(By.xpath("//select[@id='day']"));
+        WebElement state = driver.findElement(By.xpath("//select[@id='state']"));
 
         Select stateName= new Select(state);
         List<WebElement> nameList = stateName.getOptions();
 
-        for (WebElement w : nameList){
-            System.out.println(w.getText());
-        }
+//        for (WebElement w : nameList){
+//            System.out.println(w.getText());
+//        }
+        nameList.stream().forEach(t-> System.out.println(t.getText()));
+    }
 
+    @Test
+    public void getSelectedOptionsTest(){
 
+        //State dropdownindaki varsayilan secili seceneğin 'Select a State' oldugunu kontrol edelim.
+        WebElement state = driver.findElement(By.xpath("//select[@id='state']"));
+        Select dayName =new Select(state);
+        String result = dayName.getFirstSelectedOption().getText();
+        Assert.assertEquals("Select a State",result);
+    }
 
+    @After
+    public void tearDown(){
+        driver.close();
     }
 
     /*
         1. What is dropdown? Dropdown nedir?
         Dropdown liste olusturmak icin kullanilir.
+
         2. How to handle dropdown elements? Dropdown nasil automate edilir?
         -Dropdown'i Locate ederiz
         -Select objecti'i olustururum
         -Select objecti'i ile istedigim secenegi secerim
         NOT: Select object'i olusturma nedenim, dropdownlarin Select class'i ile olusturulmasi
+
         3. Tum dropdown seceneklerini nasil prin ederiz?
         -Tum dropdown elementlerini getOptions() methodu ile listeye koyariz
         -sonra secenekleri loop ile yazdirabiliriz.
+
+        4.Bir seçeneğin secili olduğunu otomate etmek icin ne yapilir?
+        Ornek : Gun olarak 10'u secdik ama ya secilmediyse ?
+
+        getFirstSelectedOption() methodu ile secili olan seceneği return eder.
 
     */
 
