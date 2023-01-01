9ced068118231b4c84bca2afda56806209f336d9
Mustafa Bacanak <bacanak117@gmail.com>
2023-01-01 20:16:46 +0300

    Maven & JUnit

--- src/test/java/homework/H01.java
+++ src/test/java/homework/H01.java
@@ -1,15 +1,43 @@
 package homework;
 
-public class H01 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
 
-/*
-    Bir class olusturun: IframeOdev
-    https://the-internet.herokuapp.com/iframe  sitesine gidiniz
-    sayfadaki toplam iframe sayısını bulunuz.
-    Sayfa basliginda ‘Editor’ yazını içerdiğini test edelim.
-    Paragrafdaki yaziyi silelim
-    Sonrasinda paragrafa “iframein icindeyim” yazisini yazdıralım
-    Alt kısımdaki yazının ‘Elemental Selenium’ yazisini içerdiğini test edelim
-*/
+import java.util.List;
+
+public class H01 extends TestBase {
+
+    @Test
+    public void homeWork(){
+
+//        https://the-internet.herokuapp.com/iframe  sitesine gidiniz
+        driver.get("https://the-internet.herokuapp.com/iframe");
+
+//        sayfadaki toplam iframe sayısını bulunuz.
+        List<WebElement> iFrameList=driver.findElements(By.xpath("//iframe"));
+        int iFrameSayi = iFrameList.size();
+
+//        Sayfa basliginda ‘Editor’ yazını içerdiğini test edelim.
+        String sayfaBaslik = driver.findElement(By.cssSelector(".example>h3")).getText();
+        Assert.assertTrue("Test Passed",sayfaBaslik.contains("Editor"));
+
+//        Paragrafdaki yaziyi silelim
+        driver.switchTo().frame(0);
+        driver.findElement(By.cssSelector("body[id='tinymce']>p")).clear();
+
+//        Sonrasinda paragrafa “iframein icindeyim” yazisini yazdıralım
+        driver.findElement(By.cssSelector("body[id='tinymce']>p")).sendKeys("iframenin icindeyim");
+
+//        Alt kısımdaki yazının ‘Elemental Selenium’ yazisini içerdiğini test edelim
+        driver.switchTo().defaultContent();
+        String altKisim = driver.findElement(By.cssSelector("div[style='text-align: center;']")).getText();
+        Assert.assertTrue(altKisim.contains("Elemental Selenium"));
+
+
+
+    }
 
 }
--- src/test/java/homework/H02.java
+++ src/test/java/homework/H02.java
@@ -1,9 +1,36 @@
 package homework;
 
-public class H02 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+import java.util.Set;
+
+public class H02 extends TestBase {
+    @Test
+    public void windowHandleTest() {
 
 //    https://testcenter.techproeducation.com/index.php?page=multiple-windows
+        driver.get("https://testcenter.techproeducation.com/index.php?page=multiple-windows");
+
 //    Title in ‘Windows’ oldugunu test edin
+        Assert.assertEquals("Windows",driver.getTitle());
+
 //    Click here a tiklayin
+        driver.findElement(By.cssSelector("a[target='_blank']")).click();
+
+//        String ilkSfHandle = driver.getWindowHandle();
+//        Set<String> allWindow= driver.getWindowHandles();
+//        for (String w : allWindow){
+//            if(!w.equals(ilkSfHandle)){
+//                driver.switchTo().window(w);
+//            }
+//        }
+
+        switchToWindow("New Window");
+
 //    Sonra açılan sayfadaki title in ‘New Window’ oldugunu dogrulayin
+        Assert.assertEquals("New Window",driver.getTitle());
+    }
 }
