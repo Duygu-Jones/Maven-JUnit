f14899a88dba6fa79cb75cce81c66a2896a4e887
Mustafa Bacanak <bacanak117@gmail.com>
2023-01-13 23:55:58 +0300

    Maven & JUnit

---
+++ src/test/java/day14/C01_Screenshot1.java
@@ -0,0 +1,55 @@
+package day14;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class C01_Screenshot1 extends TestBase {
+    @Test
+    public void fullPageScreenshotTest() throws IOException {
+
+ /*
+        Selenium tun ekran goruntusu nasil alinir?
+    -tum ekran goruntusu seleniumdan gelen getScreenshotAs metotu ile alinir.
+    -getScreenshotAs metotu seleniumdaki TakesScreenshot api indan gelir
+ */
+
+        //Techpro education a git ve Sayfanin goruntusunu al
+        driver.get("https://www.techproeducation.com");
+
+        //1. Ekran goruntusunu getScreenshotAs methodu ile alip File olarak olusturalim
+        File goruntu = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        //2. Almis olduğum ekran goruntusunu belirttiğim bir PATH'e kaydet  .dir = directory = dosya
+        String currentDate=new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        String path = System.getProperty("user.dir")+"\\test-output\\EkranGoruntuleri\\"+currentDate+"image.png";
+
+
+//      File hedef = new File(path); >> aşağıda "hedef olarak da kullanılabilirdi obje olusturmayı tercih etik"
+
+        //3.Goruntum ile dosyami birleştir
+        FileUtils.copyFile(goruntu,new File(path));
+
+        //-“QA” aramasi yap
+        findCss("input[title='Search']").sendKeys("QA", Keys.ENTER);
+
+        //Acilen sayfanin metnini test et ve ekran goruntusu al: “Search Results for: QA”
+        WebElement text = findCss("div[class='elementor-widget-container']>h1");
+//                        findXPath("//*[contains(text(),'Search Results for')]");
+
+        Assert.assertEquals("Search Results for: QA",text.getText());
+
+        takeScreenShotOfPage();
+
+    }
+}
